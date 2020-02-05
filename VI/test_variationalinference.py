e65eb8f0698541f1c958f94dbaf610e2e591bb8f
constantin <constantin.grigo@tum.de>
2020-02-05 20:25:55 +0100

    implemented and tested multivariate Gaussian stochastic variational inference

--- VI/test_variationalinference.py
+++ VI/test_variationalinference.py
@@ -27,21 +27,21 @@ class SVIGauss2Gauss1D(unittest.TestCase):
         svi.fit()
 
         print('True mean == ', mu_e)
-        print('VI mean == ', svi.vi_mean)
+        print('VI mean == ', svi.params['loc'])
         print('True std == ', sigma_e)
         print('VI std == ', svi.vi_std)
         # 3% deviation is allowed
-        self.assertLess(abs(mu_e - svi.vi_mean)/(abs(mu_e) + 1e-6), tol)
+        self.assertLess(abs(mu_e - svi.params['loc'])/(abs(mu_e) + 1e-6), tol)
         self.assertLess(abs(sigma_e - svi.vi_std)/(abs(sigma_e) + 1e-6), tol)
         print('... Gauss to Gauss VI in 1D test passed.')
 
 
-class SVIGauss2Gauss2D(unittest.TestCase):
+class SVIDiagGauss2Gauss2D(unittest.TestCase):
 
     def test_gauss2gauss_2d(self):
         # 3 percent error tolerance
         tol = 3e-2
-        print('Testing Gauss to Gauss VI in 2D...')
+        print('Testing diagonal Gauss to Gauss VI in 2D...')
         sigma_e = torch.tensor([2.0, 3.0])
         mu_e = torch.tensor([-2.0, 3.0])
 
@@ -55,11 +55,43 @@ class SVIGauss2Gauss2D(unittest.TestCase):
         svi.fit()
 
         print('True mean == ', mu_e)
-        print('VI mean == ', svi.vi_mean)
+        print('VI mean == ', svi.params['loc'])
         print('True std == ', sigma_e)
         print('VI std == ', svi.vi_std)
-        self.assertLess(abs(mu_e - svi.vi_mean)/(abs(mu_e) + 1e-6), tol)
-        self.assertLess(abs(sigma_e - svi.vi_std)/(abs(sigma_e) + 1e-6), tol)
+        self.assertLess(torch.norm(mu_e - svi.params['loc'])/(torch.norm(mu_e) + 1e-6), tol)
+        self.assertLess(torch.norm(sigma_e - svi.vi_std)/(torch.norm(sigma_e) + 1e-6), tol)
+        print('... Gauss to Gauss VI in 2D test passed.')
+
+
+class SVIMVGauss2Gauss2D(unittest.TestCase):
+
+    def test_gauss2gauss_2d(self):
+        # 3 percent error tolerance
+        tol = 3e-2
+        print('Testing multivariate Gauss to Gauss VI in 2D...')
+        L_e = torch.eye(2)
+        L_e[1, 0] = .5
+        Sigma_e = L_e @ L_e.T
+        mu_e = torch.tensor([-2.0, 3.0])
+
+        def log_emp_dist(x):
+            # Can be done more efficiently using the existing LU decompusition
+            rhs = (x - mu_e).unsqueeze(1)
+            return .5*(x - mu_e).T @ torch.cholesky_solve(rhs, L_e).squeeze()
+
+        def log_emp_dist_grad(x):
+            rhs = (x - mu_e).unsqueeze(1)
+            return torch.cholesky_solve(rhs, L_e).squeeze()
+
+        svi = vi.FullRankGaussianSVI(log_emp_dist, log_emp_dist_grad, 2)
+        svi.fit()
+
+        print('True mean == ', mu_e)
+        print('VI mean == ', svi.variationalDistribution.loc)
+        print('True Sigma == ', Sigma_e)
+        print('VI Sigma == ', svi.variationalDistribution.covariance_matrix)
+        self.assertLess(torch.norm(mu_e - svi.variationalDistribution.loc)/(torch.norm(mu_e) + 1e-6), tol)
+        self.assertLess(torch.norm(svi.variationalDistribution.scale_tril - L_e)/(torch.norm(L_e) + 1e-6), tol)
         print('... Gauss to Gauss VI in 2D test passed.')
 
 
--- VI/variationalinference.py
+++ VI/variationalinference.py
@@ -5,38 +5,40 @@ import torch
 
 
 class StochasticVariationalInference:
-    def __init__(self):
-        pass
-
-
-class DiagGaussianSVI:
     def __init__(self, log_emp_dist, log_emp_dist_grad, dim):
         self.log_emp_dist = log_emp_dist
         self.log_emp_dist_grad = log_emp_dist_grad
-        self.vi_mean = torch.zeros(dim, requires_grad=True)
-        self.vi_log_std = torch.zeros(dim, requires_grad=True)
-        self.estimate = 0
-        self.grad_estimate = torch.zeros(dim)   # w.r.t. z
+        self.dim = dim
+        self.params = {}
+        self.elbo_estimate = 0
+        self.elbo_grad_estimate = torch.zeros(self.dim)  # w.r.t. z
+
+
+class DiagGaussianSVI(StochasticVariationalInference):
+    def __init__(self, log_emp_dist, log_emp_dist_grad, dim):
+        super(DiagGaussianSVI, self).__init__(log_emp_dist, log_emp_dist_grad, dim)
+        self.params['loc'] = torch.zeros(self.dim, requires_grad=True)
+        self.params['log_std'] = torch.zeros(self.dim, requires_grad=True)
 
         self.autograd_elbo = self.get_autograd_elbo()
 
-        self.optimizer = optim.Adam([self.vi_mean, self.vi_log_std])
-        self.lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, factor=.3, patience=1e5, verbose=True)
+        self.optimizer = optim.Adam([self.params['loc'], self.params['log_std']])
+        self.lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, factor=.3, patience=1e4, verbose=True)
         self.min_lr = .01*self.optimizer.param_groups[0]['lr']
         self.max_iter = 1e8
 
     @property
     def vi_std(self):
-        return torch.exp(self.vi_log_std)
+        return torch.exp(self.params['log_std'])
 
     def evaluate(self, z):
         """
         Evaluates the elbo for a single z only!
         """
-        self.estimate = self.log_emp_dist(z) - sum(self.vi_log_std)
+        self.elbo_estimate = self.log_emp_dist(z) - sum(self.params['log_std'])
 
     def evaluate_grad(self, z):
-        self.grad_estimate = self.log_emp_dist_grad(z)
+        self.elbo_grad_estimate = self.log_emp_dist_grad(z)
 
     def get_autograd_elbo(self):
         """
@@ -54,14 +56,14 @@ class DiagGaussianSVI:
                 z = mean + std*epsilon
                 ctx.save_for_backward(z, epsilon, std)
                 self.evaluate(z)
-                return self.estimate
+                return self.elbo_estimate
 
             @staticmethod
             def backward(ctx, grad_output):
                 # grad_output = d_log_p/dz
                 z, epsilon, std = ctx.saved_tensors
                 self.evaluate_grad(z)
-                return self.grad_estimate, std*epsilon*self.grad_estimate - 1.0
+                return self.elbo_grad_estimate, std*epsilon*self.elbo_grad_estimate - 1.0
 
         return AutogradElbo.apply
 
@@ -69,7 +71,7 @@ class DiagGaussianSVI:
         converged = False
         iter = 0
         while not converged:
-            loss = self.autograd_elbo(self.vi_mean, self.vi_log_std)
+            loss = self.autograd_elbo(self.params['loc'], self.params['log_std'])
             loss.backward()
             self.optimizer.step()
             self.optimizer.zero_grad()
@@ -80,6 +82,77 @@ class DiagGaussianSVI:
             elif self.optimizer.param_groups[0]['lr'] < self.min_lr:
                 converged = True
                 print('VI converged because learning rate dropped below threshold (scheduler)')
+            else:
+                iter += 1
+
+
+class FullRankGaussianSVI(StochasticVariationalInference):
+    def __init__(self, log_emp_dist, log_emp_dist_grad, dim):
+        super(FullRankGaussianSVI, self).__init__(log_emp_dist, log_emp_dist_grad, dim)
+        loc = torch.zeros(self.dim, requires_grad=True)
+        scale_tril = torch.eye(self.dim, requires_grad=True)
+        self.variationalDistribution = torch.distributions.multivariate_normal.MultivariateNormal(
+            loc=loc, scale_tril=scale_tril)
+
+        self.autograd_elbo = self.get_autograd_elbo()
+
+        self.optimizer = optim.Adam([loc, scale_tril])
+        self.lr_scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, factor=.3, patience=1e4, verbose=True)
+        self.min_lr = .01*self.optimizer.param_groups[0]['lr']
+        self.max_iter = 1e8
+
+    def evaluate(self, z):
+        """
+        Evaluates the elbo for a single z only!
+        """
+        self.elbo_estimate = self.log_emp_dist(z) - torch.logdet(self.variationalDistribution.scale_tril)
+
+    def evaluate_grad(self, z):
+        # Gradient w.r.t. dependent variable z
+        self.elbo_grad_estimate = self.log_emp_dist_grad(z)
+
+    def get_autograd_elbo(self):
+        """
+        Creates an autograd function of elbo
+        """
+
+        class AutogradElbo(torch.autograd.Function):
+
+            @staticmethod
+            def forward(ctx, mean, scale_tril):
+                # The dependent variable epsilon is a torch.tensor with requires_grad=True;
+                # it is a standard normal random variable
+                epsilon = torch.randn_like(mean)
+                z = mean + scale_tril@epsilon
+                ctx.save_for_backward(z, epsilon, scale_tril)
+                self.evaluate(z)
+                return self.elbo_estimate
+
+            @staticmethod
+            def backward(ctx, grad_output):
+                # grad_output = d_log_p/dz
+                z, epsilon, scale_tril = ctx.saved_tensors
+                self.evaluate_grad(z)
+                return self.elbo_grad_estimate, \
+                       torch.tril(torch.ger(self.elbo_grad_estimate, epsilon) - torch.inverse(scale_tril.T))
+
+        return AutogradElbo.apply
+
+    def fit(self):
+        converged = False
+        iter = 0
+        while not converged:
+            loss = self.autograd_elbo(self.variationalDistribution.loc, self.variationalDistribution.scale_tril)
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            self.lr_scheduler.step(loss)
+            if iter > self.max_iter:
+                converged = True
+                print(f"VI converged because max number of {self.max_iter} iterations reached")
+            elif self.optimizer.param_groups[0]['lr'] < self.min_lr:
+                converged = True
+                print(f'VI converged because learning rate dropped below threshold {self.min_lr} (scheduler)')
             else:
                 iter += 1
 
